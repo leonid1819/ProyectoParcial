da00fdf421b623add77a1f56391b9ad2e9afd91c
unknown <leonidandrade123@gnail.com>
2022-05-29 18:23:38 -0500

    coneccion a sql server

--- app/bd.py
+++ app/bd.py
@@ -1,11 +1,13 @@
 import pyodbc 
 
-server='35.223.17.129'
-bd='prueba'
-userio='andradeleonid'
-contrasenia='pO(>$zKJ~nG/Q5D~'
 
-conexion=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+';DATABASE='+bd+';UID='+userio+';PWD='+contrasenia+'')
-print(conexion)
+def obtener_conexion():
+    server='35.223.17.129'
+    bd='prueba'
+    userio='sqlserver'
+    contrasenia='pO(>$zKJ~nG/Q5D~'
+
+    return pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+';DATABASE='+bd+';UID='+userio+';PWD='+contrasenia+'')
+    
 
 
