fb0a9726b9e09af6dc66e5707e8f8402a0e0b652
unknown <leonidandrade123@gnail.com>
2022-05-29 15:14:14 -0500

    :,v

--- app/bd.py
+++ app/bd.py
@@ -1,1 +1,11 @@
+import pyodbc 
+
+server='35.223.17.129'
+bd='prueba'
+userio='andradeleonid'
+contrasenia='pO(>$zKJ~nG/Q5D~'
+
+conexion=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+';DATABASE='+bd+';UID='+userio+';PWD='+contrasenia+'')
+print(conexion)
+
 
